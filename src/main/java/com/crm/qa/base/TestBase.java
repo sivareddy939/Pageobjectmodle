402045085e5beca7e88e2f4175c436dcfc113434
sudhakar <sudhakargruka.007@gmail.com>
2018-11-01 22:30:27 +0530

    ih

--- src/main/java/com/crm/qa/base/TestBase.java
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -28,7 +28,6 @@ public class TestBase {
 		
 		try{
 			File src = new File("C:\\sivareddyproject\\framework\\src\\main\\java\\com\\crm\\qa\\config\\config.properties");
-			
 			FileInputStream fis = new FileInputStream(src);
 			prop = new Properties();
 			prop.load(fis);	
